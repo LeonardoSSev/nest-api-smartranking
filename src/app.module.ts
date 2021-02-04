21f8c8ef2fb4f031a9493bc4c1c2d814c054958e
leonardossev <leonardossev@gmail.com>
2021-02-04 00:47:09 -0300

    config: Sets the env variables for database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { PlayersController } from './players/players.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [PlayersModule],
-  controllers: [PlayersController],
+  imports: [
+    MongooseModule.forRoot(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false 
+    }),
+    PlayersModule],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
