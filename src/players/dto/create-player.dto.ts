31dbcdb70be91177c4764329d51b1b9a25adaca8
leonardossev <leonardossev@gmail.com>
2021-02-03 23:26:04 -0300

    feat: Adds Player's interface and creation DTO

---
+++ src/players/dto/create-player.dto.ts
@@ -0,0 +1,7 @@
+export class CreatePlayerDTO {
+  readonly phoneNumber: string;
+
+  readonly email: string;
+
+  readonly name: string;
+}
---
+++ src/players/interfaces/player.interface.ts
@@ -0,0 +1,15 @@
+export interface Player {
+  readonly _id: string;
+
+  readonly phoneNumber: string;
+
+  readonly email: string;
+
+  name: string;
+
+  ranking: string;
+
+  rankingPosition: number;
+
+  urlPicture: string;
+}
